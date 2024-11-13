4a4780dc9f31dfecc41ddfba64cd8c5c0b15ec1d
Александр Семёнов <aleksandrsemenov@iMac-Aleksandr.local>
2024-11-13 23:51:21 +0300

    lesson 3 - 4/4

--- SwiftLearn.playground/Pages/lesson 3.xcplaygroundpage/Contents.swift
+++ SwiftLearn.playground/Pages/lesson 3.xcplaygroundpage/Contents.swift
@@ -20,21 +20,19 @@ import Foundation
 //print(twoEqualNumbers(firstNumber: 19, secondNumber: 8))
 
 // НЕ РАБОТАЕТ
-//func twoEqualNumbers(firstNumber: Int, secondNumber: Int) -> String {
-//    let result: String
-//    switch (firstNumber, secondNumber) {
+//func twoEqualNumbers(firstNumber: Int, secondNumber: Int) {
+//    switch firstNumber; secondNumber {
 //    case firstNumber == secondNumber:
-//        result = "равно"
+//        print("равно")
 //    case firstNumber > secondNumber:
-//        result = "больше"
+//        print("больше")
 //    case firstNumber < secondNumber:
-//        result = "меньше"
+//        print("меньше")
 //    default:
-//        break
+//        break 
 //    }
-//    return result
 //}
-//print(twoEqualNumbers(firstNumber: 19, secondNumber: 8))
+//print(twoEqualNumbers(firstNumber: 13, secondNumber: 7))
 
 
 
@@ -61,6 +59,7 @@ import Foundation
 //print(summ(toValue: 15))
 
 
+
 //Задание 3
 //Напишите функцию, которая будет принимать целое число, а выводить все целые числа, квадраты которых меньше или равны переданному параметру. Квадрат числа — это результат умножения числа на самого себя. Например, 9 — это квадрат числа 3.
 // n = 10
@@ -82,6 +81,7 @@ import Foundation
 //print(squareNumbers(number: 225))
 
 
+
 //Задание 4
 //Напишите функцию func handleDiceRoll(result: Int) для обработки значений одного брошенного кубика с помощью оператора switch. На вход функция получает число от 1 до 6 включительно. Если значение 1, 2 или 3, нужно вывести в консоль слово «проигрыш». Если значение 4, 5 или 6 — слово «победа».
 //
@@ -90,11 +90,14 @@ import Foundation
 // handleDiceRoll(result: 5)
 // Вывод в консоль: Победа
 
-func handleDiceRoll(result: Int) {
-    switch result {
-    case result == (1...3):
-        print("проигрыш")
-    case result == (4...6):
-        print("победа")
-    }
-}
+//func handleDiceRoll(result: Int) {
+//    switch result {
+//    case 1...3:
+//        print("проигрыш")
+//    case 4...6:
+//        print("победа")
+//    default:
+//        break
+//    }
+//}
+//print(handleDiceRoll(result: 5))
