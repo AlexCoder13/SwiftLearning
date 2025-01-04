4472457b69195d14f453b47f5e9b8863ee9e8d55
AlexCoder13 <sesasha99@gmail.com>
2025-01-04 15:50:12 +0300

    OOP - не работает ретерн в библиотеку

--- SwiftLearn.playground/Pages/OOP HOMEWORK.xcplaygroundpage/Contents.swift
+++ SwiftLearn.playground/Pages/OOP HOMEWORK.xcplaygroundpage/Contents.swift
@@ -17,8 +17,8 @@ import Foundation
 //Метод returnBook(userID: Int, bookTitle: String), чтобы вернуть книгу.
 //В классе LibraryUser:
 //Метод listBorrowedBooks() для вывода списка всех взятых книг.
-//Добавьте проверку:
 //
+//Добавьте проверку:
 //Если книги нет в библиотеке, выдайте ошибку.
 //Если пользователь пытается взять больше 3 книг, отклоните запрос.
 
@@ -39,7 +39,7 @@ class Book {
 class LibraryUser {
     let name: String
     let userID: Int
-    var borrowedBooks: [Book]
+    private var borrowedBooks: [Book]
     
     init(name: String, userID: Int) {
         self.name = name
@@ -48,18 +48,33 @@ class LibraryUser {
     }
     
     func listBorrowedBooks() {
-        print(borrowedBooks)
+        for book in borrowedBooks {
+            print(book.title, book.author, book.genre, book.year)
+        }
     }
+    
+    func borrowBook(book: Book) {
+        if borrowedBooks.count >= 3 {
+            print("Слишком много книг. Верни одну в библиотеку.")
+        }
+        borrowedBooks.append(book)
+    }
+    
+    func removeBook(title: String) {
+        borrowedBooks.removeAll { $0.title == title }
+//        guard let book = getBorrowedBook(by: title) else { return }
+//        books.append(book)
+    }
+    
+//    private func getBorrowedBook(by title: String) -> Book? {
+//        guard let book = borrowedBooks.first(where: {$0.title == title}) else { return nil }
+//        return book
+//    }
 }
 
 class Library {
-    var books: [Book]
-    var users: [LibraryUser]
-    
-    init() {
-        self.books = []
-        self.users = []
-    }
+    var books: [Book] = []
+    var users: [LibraryUser] = []
     
     func addBook(book: Book) {
         books.append(book)
@@ -70,18 +85,57 @@ class Library {
     }
     
     func borrowBook(userID: Int, bookTitle: String) {
+        guard let user = getUser(by: userID) else { return }
+        guard let book = getBook(by: bookTitle) else { return }
         
+        user.borrowBook(book: book)  // тут тоже метод а не свойство
+        books.removeAll { $0.title == bookTitle }
     }
     
     func returnBook(userID: Int, bookTitle: String) {
+        guard let user = getUser(by: userID) else { return }
         
+        user.removeBook(title: bookTitle) // это метод а не свойство
+    }
+    
+    private func getUser(by userID: Int) -> LibraryUser? {
+        guard let user = users.first (where: { $0.userID == userID }) else { return nil }
+        return user
+    }
+    
+    private func getBook(by title: String) -> Book? {
+        guard let book = books.first (where: { $0.title == title }) else { return nil }
+        return book
     }
 }
 
+let firstBook = Book(title: "Война и Мир", author: "Лев Толстой", genre: "роман-эпопея", year: 1869)
+let secondBook = Book(title: "Преступление и Наказание", author: "Фёдор Достоевский", genre: "роман", year: 1866)
+let thirdBook = Book(title: "Гарри Поттер и Философский камень", author: "Дж. К. Роулинг", genre: "фентези", year: 1997)
+let forthBook = Book(title: "Гарри Поттер и Тайная комната", author: "Дж. К. Роулинг", genre: "фентези", year: 1998)
+
+let firstUser = LibraryUser(name: "Alex", userID: 1)
+let secondUser = LibraryUser(name: "Nickolay", userID: 2)
+let thirdUser = LibraryUser(name: "Danila", userID: 3)
 
 
+let library = Library()
+library.books.append(firstBook)
+library.books.append(secondBook)
+library.books.append(thirdBook)
+library.books.append(forthBook)
 
+library.users.append(firstUser)
+library.users.append(secondUser)
+library.users.append(thirdUser)
 
+library.books
+library.borrowBook(userID: 2, bookTitle: "Война и Мир")
+secondUser.listBorrowedBooks()
+
+library.returnBook(userID: 2, bookTitle: "Война и Мир")
+library.books
+secondUser.listBorrowedBooks()
 
 
 
