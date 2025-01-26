98a3a418df7a86acb90cbc41ae9d0bf858bc20fe
AlexCoder13 <sesasha99@gmail.com>
2025-01-26 11:24:06 +0300

    functions - 9/9

--- SwiftLearn.playground/Pages/Functions HW.xcplaygroundpage/Contents.swift
+++ SwiftLearn.playground/Pages/Functions HW.xcplaygroundpage/Contents.swift
@@ -82,16 +82,67 @@ print("-------------------------")
 //Функция должна возвращать словарь с теми же студентами, но в качестве значения должен выступать статус либо hight либо low (для этого можно определить перечисление), если у студента баллы выше или равны 90 то мы для него определяем группу hight, если ниже то low.
 //Присвойте результат работы функции новому свойству.
 
+let students = ["Tim": Int.random(in: 1...100),
+                "Sem": Int.random(in: 1...100),
+                "Taylor": Int.random(in: 1...100)]
+
+enum Status {
+    case high
+    case low
+}
+
+func statusOfStudents(students: [String: Int]) -> [String: Status] {
+    var statusOfStudents: [String: Status] = [:]
+    for (key, value) in students {
+        if value >= 90 {
+            statusOfStudents[key] = .high
+        } else {
+            statusOfStudents[key] = .low
+        }
+    }
+    return statusOfStudents
+}
+
+let statusStudents = statusOfStudents(students: students)
+for (key, value) in statusStudents {
+    print("\(key): \(value)")
+}
+print("-------------------------")
+
 //7.
 //Реализуйте функцию, которая принимает три параметра: цена за единицу товара, количество товаров и скидка в процентах. Функция должна возвращать общую стоимость заказа, после применения скидки.
 //Если значение параметра со скидкой не передано в функцию, то функция должна применять скидку в размере  0%
 //Все параметры должны иметь тип Double.
+
+func costOfOrder(costOfItem: Double, countOfItems: Double, discont: Double?) -> Double {
+    let fullCostOfOrder = costOfItem * countOfItems
+    let doubleDiscont: Double = discont ?? 0
+    return fullCostOfOrder - (fullCostOfOrder * (doubleDiscont / 100))
+}
+
+print(costOfOrder(costOfItem: 100, countOfItems: 10, discont: 25))
+print("-------------------------")
+
 
 //8.
 //Создайте функцию, которая принимает в качестве параметров любое количество направлений (север, юг, запад и восток) и выводит на консоль сообщение о том в каком направлении находится пользователь. Ваша задача вызвать функцию с несколькими значениями направления, например если мы передадим в функцию значение север и юг, то должны увидеть на консоли сообщения:
 // Вывод:
 // Heading north
 // Heading south
+
+var arrayOfDirections: [String] = []
+
+func checkDirection(directions: [String]) {
+    for element in directions {
+        print("Heading \(element)")
+    }
+}
+
+arrayOfDirections.append("north")
+arrayOfDirections.append("west")
+
+checkDirection(directions: arrayOfDirections)
+print("-------------------------")
 
 //9.
 //Создайте функцию, которая будет удваивать переданное ей значение типа Int, и возвращать этот результат.
@@ -104,3 +155,16 @@ print("-------------------------")
 // Operation result after iteration 3 is: 24
 // Operation result after iteration 4 is: 48
 // Operation result after iteration 5 is: 96
+
+func doubleNum(num: Int) -> Int{
+    num * 2
+}
+
+func updateDoubleNum(num: Int, count: Int, _ operation: (Int) -> Int) {
+    var result: Int
+    result = count * operation(num)
+    print("Operation result after iteration \(count) is: \(result)")
+}
+
+updateDoubleNum(num: 3, count: 5, doubleNum(num:))
+print("-------------------------")
