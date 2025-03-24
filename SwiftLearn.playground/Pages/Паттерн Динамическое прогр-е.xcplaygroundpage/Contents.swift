67dfd6373a025a8bd18760e453e901b119d58881
AlexCoder13 <sesasha99@gmail.com>
2025-03-24 10:39:02 +0300

    п. динам-е прогр-е - пример 3

--- SwiftLearn.playground/Pages/Паттерн Динамическое прогр-е.xcplaygroundpage/Contents.swift
+++ SwiftLearn.playground/Pages/Паттерн Динамическое прогр-е.xcplaygroundpage/Contents.swift
@@ -62,4 +62,30 @@ let capacity = 5
 print(knapsack(weights, values, capacity)) // Вывод: 7
 
 
+//Пример 3: Наибольшая общая подпоследовательность (LCS). Задача: Найти длину наибольшей общей подпоследовательности двух строк.
+func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+    let m = text1.count
+    let n = text2.count
+    let array1 = Array(text1)
+    let array2 = Array(text2)
+    var dp = [[Int]](repeating: [Int](repeating: 0, count: n + 1), count: m + 1)
+    
+    for i in 1...m {
+        for j in 1...n {
+            if array1[i - 1] == array2[j - 1] {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+    
+    return dp[m][n]
+}
+
+// Пример использования:
+let text1 = "abcde"
+let text2 = "ace"
+print(longestCommonSubsequence(text1, text2)) // Вывод: 3
+
 
