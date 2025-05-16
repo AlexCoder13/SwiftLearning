6ffc474150f4fad07855f8a8def1b369819d5b2e
AlexCoder13 <sesasha99@gmail.com>
2025-05-17 00:01:19 +0300

    bdui - p.3

--- GCD.playground/Pages/BDUI.xcplaygroundpage/Contents.swift
+++ GCD.playground/Pages/BDUI.xcplaygroundpage/Contents.swift
@@ -63,4 +63,47 @@ class UIFactory {
     }
 }
 
-//
+// 3. ViewController
+class DynamicViewController: UIViewController {
+    // 1. Текущее состояние
+    private var currentState: ScreenState
+    // 2. UI-элементы
+    private lazy var titleLabel = UILabel()
+    private lazy var actionButton = UIButton()
+    private lazy var textField = UITextField()
+    // 3. Инициализатор с состоянием
+    init(state: ScreenState) {
+        self.currentState = state
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        render(state: currentState)
+    }
+    // 4. Настройка лейаута
+    private func setupUI() {
+        view.backgroundColor = .white
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, textField, actionButton])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+    // 5. Рендер состояния
+    func render(state: ScreenState) {
+        titleLabel.text = state.title
+        actionButton = UIFactory.createButton(from: state.buttonState)
+        textField = UIFactory.createTextField(from: state.textFieldState)
+    }
+}
+
+// 
