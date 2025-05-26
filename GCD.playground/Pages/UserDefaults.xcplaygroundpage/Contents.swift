82aa6ed605acf416f49347b92aaf4c6bb479babd
AlexCoder13 <sesasha99@gmail.com>
2025-05-27 00:01:24 +0300

    UserDefaults - pt.1

---
+++ GCD.playground/Pages/UserDefaults.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+// MARK: - UserDefaults
+
+/*
+ Описание:
+ UserDefaults — это встроенный механизм в iOS/macOS для хранения небольших объемов данных (настроек, флагов, конфигураций) между запусками приложения. Это простой key-value storage, который сохраняет данные в .plist-файле.
+ */
+
+// 1. Стандартный доступ
+// Запись
+UserDefaults.standard.set("Алексей", forKey: "username")
+UserDefaults.standard.set(25, forKey: "userAge")
+UserDefaults.standard.set(true, forKey: "isDarkMode")
+// Чтение
+let username = UserDefaults.standard.string(forKey: "username") ?? "Гость"
+let age = UserDefaults.standard.integer(forKey: "userAge")
+let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+print(username) // "Алексей"
+print(age)      // 25
+print(isDarkMode) // true
